068651feb5da9dfdf907cad0c98669a956aafae5
T3stzer0 <shiyang.wei@chaitin.com>
2024-06-22 21:57:18 +0800

    add LISCENSE

--- setup.py
+++ setup.py
@@ -1,5 +1,31 @@
 # setup.py
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
+import os
+
+
+def read(fname):
+    return open(
+        os.path.join(os.path.dirname(__file__), fname), "r", encoding="utf-8"
+    ).read()
+
+
+NAME = "reprb"
+
+DESCRIPTION = "Represent bytes with printable characters"
+
+LONG_DESCRIPTION = read("README.MD")
+
+KEYWORDS = "repr bytes eval dump load"
+
+AUTHOR = "T3stzer0"
+
+AUTHOR_EMAIL = "testzero.wz@gmail.com"
+
+URL = "https://github.com/testzero-wz/reprb"
+
+VERSION = "1.0.0"
+
+LICENSE = "MIT"
 
 module = Extension(
     "_reprb",
@@ -8,9 +34,18 @@ module = Extension(
 )
 
 setup(
-    name="reprb",
-    version="1.0",
-    author="T3stzer0",
-    description="represent bytes as printable chars, vice versa",
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
     ext_modules=[module],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
 )
